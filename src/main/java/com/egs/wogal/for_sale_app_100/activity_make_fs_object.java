0d785918e86aa0611d38cf1e1716163289607e4e
wogal <egilgeous@gmail.com>
2017-03-17 07:51:23 -0400

    Default task

---
+++ src/main/java/com/egs/wogal/for_sale_app_100/activity_make_fs_object.java
@@ -0,0 +1,23 @@
+package com.egs.wogal.for_sale_app_100;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+
+public class activity_make_fs_object extends AppCompatActivity {
+
+    Toolbar hhl;   // wogal
+    EditText ggg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_fs_object);
+    }
+
+    /**
+     * Created by wogal on 3/14/2017.
+     */
+    //    kkkkkk
+    public static class file_storage_helper_100 {
+    }
+}
